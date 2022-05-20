8becc17cacba6bb00d7594484941a29d315df1ad
rc143621 <104138637+rc143621@users.noreply.github.com>
2022-05-20 15:14:24 +0200

    Update translation.go

--- translation.go
+++ translation.go
@@ -4,7 +4,6 @@ package entity
 
 // Translation -.
 type Translation struct {
-	Source      string `json:"source"       example:"auto"`
 	Destination string `json:"destination"  example:"en"`
 	Original    string `json:"original"     example:"текст для перевода"`
 	Translation string `json:"translation"  example:"text for translation"`
